c729733d12703fefd8ee2a1e5e9da35a829072b6
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2024-12-23 07:03:58 -0300

    fix food card skeleton style

--- src/pages/foods/components/food-card-skeleton.tsx
+++ src/pages/foods/components/food-card-skeleton.tsx
@@ -1,14 +1,12 @@
-import { Skeleton } from "@/components/ui/skeleton"
+import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function FoodCardSkeleton() {
   return (
-    <div className="flex items-center space-x-4 p-4 border rounded-lg">
-      <Skeleton className="h-12 w-12 rounded-full" />
-      <div className="space-y-2 flex-1">
-        <Skeleton className="h-4 w-3/4" />
-        <Skeleton className="h-4 w-1/2" />
-      </div>
-      <Skeleton className="h-6 w-16" />
-    </div>
-  )
+    <Card className="p-4 space-y-2">
+      <Skeleton className="h-5 w-3/4" />
+      <Skeleton className="h-4 w-1/2" />
+      <Skeleton className="h-4 w-1/3" />
+    </Card>
+  );
 }
